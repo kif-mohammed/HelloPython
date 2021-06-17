4f0320fa60537625e538b688965dec43421f9e90
kif-mohammed <85088553+kif-mohammed@users.noreply.github.com>
2021-06-16 21:35:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,20 @@ def hello2():
     return "Hello2 again, Universal!"
 
 
+incomes = [
+  { 'description': 'salary', 'amount': 5000 }
+]
+
+
+@app.route('/incomes')
+def get_incomes():
+  return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+  incomes.append(request.get_json())
+  return '', 204
+
 if __name__ == '__main__':
     api.run()
