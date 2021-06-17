961b5adcf068c36caab09a9a8689c538e89061d3
kif-mohammed <85088553+kif-mohammed@users.noreply.github.com>
2021-06-16 20:50:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, json
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,3 +8,9 @@ def hello():
 @app.route("/Hello")
 def hello2():
     return "Hello2 again, Universal!"
+
+companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
+
+@api.route('/companies', methods=['GET'])
+def get_companies():
+  return json.dumps(companies)
